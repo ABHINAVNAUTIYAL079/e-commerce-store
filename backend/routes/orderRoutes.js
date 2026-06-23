8c40eb0874103daa2b54a6a4b204cf792e376afa
ABHINAVNAUTIYAL079 <abhinavnautiyal123456@gmail.com>
2026-06-23 18:11:59 +0530

    implement order routes backend

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  authenticateUser,
+  authorizeAdmin,
+} from "../middlewares/authMiddleware.js";
+import {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calculateTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} from "../controllers/orderController.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(authenticateUser, createOrder)
+  .get(authenticateUser, authorizeAdmin, getAllOrders);
+
+router.route("/mine").get(authenticateUser, getUserOrders);
+
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calculateTotalSalesByDate);
+router.route("/:id").get(authenticateUser, findOrderById);
+router.route("/:id/pay").put(authenticateUser, markOrderAsPaid);
+router
+  .route("/:id/deliver")
+  .put(authenticateUser, authorizeAdmin, markOrderAsDelivered);
+
+export default router;
